5210de969f42ded449588937e6f3332264465a14
Alexandru Badita <alexandru360@gmail.com>
2020-08-15 19:58:35 +0300

    thumbnails controller refactoring

---
+++ api/DTO/ThumbnailPathsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DTO
+{
+    public class ThumbnailPathsDto
+    {
+        public string ThumbnailOnePath { get; set; } = string.Empty;
+        public string ThumbnailTwoPath { get; set; } = string.Empty;
+    }
+}
